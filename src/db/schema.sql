1b2db51ee1bb735750e22312e36f3b482727fb97
mhorton-dev <michael@mhorton.dev>
2025-09-23 08:53:42 -0500

    Updated create table statement for user_bets

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -4,7 +4,9 @@ DROP TABLE IF EXISTS teams CASCADE;
 
 DROP TABLE IF EXISTS games;
 
-DROP TABLE IF EXISTS scoreboards;
+DROP TABLE IF EXISTS users;
+
+DROP TABLE IF EXISTS user_bets;
 
 
 CREATE TABLE
@@ -26,7 +28,7 @@ CREATE TABLE
 CREATE TABLE
     games (
         id SERIAL PRIMARY KEY,
-        game_id INT,
+        game_id INT UNIQUE,
         season INT,
         season_week INT,
         season_type TEXT,
@@ -42,7 +44,7 @@ CREATE TABLE
         away_qtr_scores INT []
     );
 
-CREATE TABLE scoreboards (
+/* CREATE TABLE scoreboards (
     game_id INT PRIMARY KEY,
     start_date DATE,
     start_time_tbd BOOLEAN,
@@ -59,18 +61,31 @@ CREATE TABLE scoreboards (
     away_team JSON,
     weather JSON,
     betting JSON
+); */
+CREATE TABLE users (
+    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL, 
+    password VARCHAR(50) NOT NULL,
+    favorite_team INT REFERENCES teams(id) ON DELETE SET NULL,
+    favorite_conference TEXT,
+    bets JSON,
+    active BOOLEAN DEFAULT true
+    );
+
+CREATE TABLE user_bets (
+    id  UUID DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL,
+    game_id INT NOT NULL,
+    amount INT,
+    spread INT,
+    bet_created TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_user_id
+        FOREIGN KEY (user_id)
+        REFERENCES users(user_id),
+    CONSTRAINT fk_game_id
+        FOREIGN KEY (game_id)
+        REFERENCES games(game_id)
 );
---     CREATE TABLE users (
---     id SERIAL PRIMARY KEY,
---     username VARCHAR(50) UNIQUE NOT NULL,
---     email VARCHAR(100) UNIQUE NOT NULL, 
---     favorite_team REFERENCES teams(id) ON DELETE SET NULL,
--- );
-
-
--- CREATE TABLE favorites (
---     user_id INT REFERENCES users(id) ON DELETE CASCADE,
---     team_id INT REFERENCES teams(id) ON DELETE CASCADE,
---     PRIMARY KEY (user_id, team_id)
--- );
 COMMIT;
