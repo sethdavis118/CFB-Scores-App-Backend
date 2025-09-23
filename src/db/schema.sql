45f3035f48abb47afd72853f172ab56df532f778
MoonMan120 <97407506+MoonMan120@users.noreply.github.com>
2025-09-23 12:43:13 -0500

    Update schema

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,80 +1,108 @@
-import db from "./client.js";
-import { createTeam } from "../queries/teams.js";
-import { createGame } from "../queries/games.js";
-import { createScoreboard } from "../queries/scoreboards.js";
-import teamList from "../../CFDTeams.js";
-import gameList from "../../CFDGames.js";
-import scoreboardList from "../../CFDScoreboard.js";
-import conferenceList from "../../CFDConferences.js";
-//import gameList from "../../ncaafevents.json";
-//import oddsList from "../../oddsspreads.json";
-//import { Register } from "#db/queries/users";
-await db.connect();
-await seed();
-await db.end();
-console.log("🌱 Database seeded.");
-async function seed() {
-  await seedTeams();
-  await seedGames();
-  /* await seedScoreboards();*/
-}
+DROP TABLE IF EXISTS teams CASCADE;
 
-async function seedTeams() {
-  try {
-    for (const team of teamList) {
-      await createTeam(
-        Number(team.id),
-        team.school,
-        team.mascot,
-        team.abbreviation,
-        team.division,
-        team.conference,
-        team.classification,
-        team.color,
-        team.alternateColor,
-        team.logos,
-        team.location.id
-      );
-    }
-  } catch (e) {
-    console.error(e);
-  }
-}
+DROP TABLE IF EXISTS games;
 
-async function seedGames() {
-  try {
-    for (const game of gameList) {
-      await createGame(
-        game.id,
-        game.season,
-        game.week,
-        game.seasonType,
-        game.startDate,
-        game.completed,
-        game.neutralSite,
-        game.conferenceGame,
-        game.homeId,
-        game.homePoints ? null : 0,
-        game.homeLineScores ? null : 0,
-        game.awayId,
-        game.awayPoints ? null : 0,
-        game.awayLineScores
-      );
-    }
-  } catch (e) {
-    console.error(e);
-  }
-}
+DROP TABLE IF EXISTS users;
 
-async function seedScoreboards() {
-  // try {
-  for (const sb of scoreboardList) {
-    console.log(`scoreboard.id ${sb.id}`);
-    await createScoreboard(sb);
-  }
-  //
+DROP TABLE IF EXISTS user_bets CASCADE;
 
-  // console.error(e);
-  // }
-}
-// //
+-- DROP TABLE IF EXISTS bets;
+
+DROP TABLE IF EXISTS rankings;
+
+-- DROP TABLE IF EXISTS favorites;
+
+
+CREATE TABLE
+    teams (
+        id SERIAL PRIMARY KEY,
+        team_id INT,
+        school TEXT,
+        mascot TEXT,
+        abbreviation TEXT,
+        conference TEXT,
+        division TEXT,
+        classification TEXT,
+        color TEXT,
+        alternate_color TEXT,
+        logos JSON,
+        home_location_id INT
+    );
+CREATE TABLE
+    games (
+        id SERIAL PRIMARY KEY,
+        game_id INT UNIQUE,
+        season INT,
+        season_week INT,
+        season_type TEXT,
+        start_date DATE,
+        completed BOOLEAN,
+        neutral_site BOOLEAN,
+        conference_game BOOLEAN,
+        home_team_id INT NOT NULL,
+        home_points INT NOT NULL,
+        home_qtr_scores INT [],
+        away_team_id INT NOT NULL,
+        away_points INT NOT NULL,
+        away_qtr_scores INT []
+    );
+
+CREATE TABLE users (
+    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL, 
+    password VARCHAR(50) NOT NULL,
+    favorite_team INT REFERENCES teams(id) ON DELETE SET NULL,
+    favorite_conference TEXT,
+    bets JSON,
+    active BOOLEAN DEFAULT true
+    );
+
+CREATE TABLE user_bets (
+    id  UUID DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL,
+    game_id INT NOT NULL,
+    amount INT,
+    spread INT,
+    bet_created TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_user_id
+        FOREIGN KEY (user_id)
+        REFERENCES users(user_id),
+    CONSTRAINT fk_game_id
+        FOREIGN KEY (game_id)
+        REFERENCES games(game_id)
+);
+
+    
+
+--     CREATE TABLE bets (
+--     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+--     user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+--     game_id UUID REFERENCES games(id) ON DELETE CASCADE,
+--     wager_amount NUMERIC(10,2) NOT NULL, -- amount user is betting
+--     team_id INT REFERENCES teams(id),    -- which team the user is betting on
+--     spread NUMERIC(5,2),                 -- e.g. -7.5, +3.0
+--     odds NUMERIC(6,3),                   -- e.g. -110, +150
+--     created_at TIMESTAMP DEFAULT NOW()
+-- );
+
+
+
+CREATE TABLE rankings (
+    id SERIAL PRIMARY KEY,
+    poll TEXT,
+    rank INT,
+    team_id INT REFERENCES teams(id),
+    school TEXT,
+    conference TEXT,
+    firstPlaceVotes INT,
+    points INT
+);
+
+
+-- CREATE TABLE favorites (
+--     user_id INT REFERENCES users(id) ON DELETE CASCADE,
+--     team_id INT REFERENCES teams(id) ON DELETE CASCADE,
+--     PRIMARY KEY (user_id, team_id)
+-- );
