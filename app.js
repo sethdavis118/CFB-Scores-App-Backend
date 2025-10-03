8cc7e7f458c07e32c1b3ee4b13152f4d05ea797e
Seth Davis <sethdavis118@icloud.com>
2025-10-03 12:23:53 -0500

    Removing /

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(
       credentials: true,
     },
     {
-      origin: "https://sidelinecfb.netlify.app/",
+      origin: "https://sidelinecfb.netlify.app",
       credentials: true,
     }
   )
